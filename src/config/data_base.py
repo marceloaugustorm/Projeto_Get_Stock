57270078b7f85433d3392d20d39635a968f07ea6
Marcelo Augusto Ribeiro Martins <2400507@SALAS.AULAS>
2025-08-20 20:31:25 -0300

    mudando banco

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -6,8 +6,7 @@ def init_db(app):
     """
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
     """
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://gestor:1234@localhost:3306/market_management"
+    # Troque a URI do MySQL por SQLite:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market_management.db"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    
-
