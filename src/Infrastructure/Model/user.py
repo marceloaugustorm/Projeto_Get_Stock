cb8cfd855e6a1ef18ee9a07b18f2c664078be623
Lucas Curtolo Belem <2400206@SALAS.AULAS>
2025-09-03 21:19:50 -0300

    Arrumando o Banco de Dados

--- src/Infrastructure/Model/user.py
+++ src/Infrastructure/Model/user.py
@@ -1,13 +1,16 @@
 from src.config.data_base import db 
 class User(db.Model):
-    __tablename__ = 'users'
+    _tablename_ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     cnpj = db.Column(db.String(14), nullable=False)
     celular = db.Column(db.String(15), nullable=False)
-    status = db.Column(db.String(100), nullable=False)
+    codigo_validacao = db.Column(db.String(10), nullable=True)
+    status = db.Column(db.Boolean, default=False, nullable=False)
+
+    
 
 
     def to_dict(self):
@@ -18,5 +21,6 @@ class User(db.Model):
             "password": self.password,
             "cnpj": self.cnpj,
             "celular": self.celular,
+            "codigo_validacao": self.codigo_validacao,
             "status": self.status
         }
