e34d1858a8da17154e1c8c04ac4a248ed306e0af
Lucas Curtolo Belem <lucasc.belem@gmail.com>
2025-10-04 23:39:42 -0300

    Arrumando para rodar o front e back juntos

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from src.config.data_base import init_db, db
 from src.routes import init_routes
 from src.Infrastructure.Model.user import User  
@@ -9,6 +10,14 @@ def create_app():
     Cria e configura a aplicação Flask.
     """
     app = Flask(__name__)
+
+    CORS(app, resources={
+        r"/*": {
+            "origins": ["http://localhost:3000"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"]
+        }
+    })
     
     # Configura o JWT
     app.config["JWT_SECRET_KEY"] = "flaroque"  
