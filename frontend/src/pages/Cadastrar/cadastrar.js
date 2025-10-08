05adb3010e11d35cc0fbd4283daad4fea2643732
Lucas Curtolo Belem <lucasc.belem@gmail.com>
2025-10-07 21:48:58 -0300

    Realizando o cadastro do user com front

--- frontend/src/pages/Cadastrar/cadastrar.js
+++ frontend/src/pages/Cadastrar/cadastrar.js
@@ -1,5 +1,65 @@
+import "./cadastrar.css";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Cadastro() {
-  return <h1>Página de Cadastro</h1>;
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cnpj: "",
+    celular: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Enviando dados:", formData);
+
+    try{
+      const response = await fetch('http://localhost:5000/user', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(formData),
+      });
+
+      if(response.ok){
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/cod");
+      } else{
+        alert("Erro ao cadastrar usuário!");
+      }
+    } catch (error){
+      console.error("Erro na requisição:", error)
+      alert("Falha na comunicação com o servidor.")
+    }
+    
+  };
+
+  return (
+    <div className="container">
+      <form id="cadastro_form" onSubmit={handleSubmit}>
+      <h2>Informações Pessoais</h2>
+      <div className="grupo">
+        <input name="name" placeholder="Nome" value={formData.name} onChange={handleChange}/>
+        <input name="email" placeholder="E-mail" value={formData.email} onChange={handleChange}/>
+        <input name="password" placeholder="Senha" value={formData.password} onChange={handleChange}/>
+        <input name="cnpj" placeholder="CNPJ" value={formData.cnpj} onChange={handleChange}/>
+        <input name="celular" placeholder="Celular" value={formData.celular} onChange={handleChange}/>
+      </div>
+      <button type="submit">Cadastrar</button>
+
+      </form> 
+
+
+    </div>
+    
+  );
 }
 
 export default Cadastro;
