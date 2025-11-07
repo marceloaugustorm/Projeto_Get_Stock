95f815cd15c38e82e27c8975c2a4c595d2d3eb0d
Marcelo Augusto Ribeiro Martins <2400507@salas.aulas>
2025-11-07 18:32:15 -0300

    bd supabase

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -1,30 +1,20 @@
-import os
 from flask_sqlalchemy import SQLAlchemy
-import socket
-
-# Opcional: força IPv4
-original_getaddrinfo = socket.getaddrinfo
-def patched_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0):
-    return original_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
-socket.getaddrinfo = patched_getaddrinfo
+import os
 
 db = SQLAlchemy()
 
 def init_db(app):
+    """
+    Inicializa a base de dados com o app Flask e o SQLAlchemy.
+    """
     database_url = os.getenv("DATABASE_URL")
-    if database_url:
-        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-            'pool_pre_ping': True,
-            'pool_recycle': 3600,
-            'pool_size': 5,
-            'max_overflow': 10,
-            'connect_args': {'connect_timeout': 10}
-        }
-    else:
-        base_dir = os.path.abspath(os.path.dirname(__file__))
-        db_path = os.path.join(base_dir, "market_management.db")
-        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 
+    if not database_url:
+        raise ValueError("Variável de ambiente DATABASE_URL não definida.")
+    
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql+psycopg2://", 1)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
