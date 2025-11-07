10fb019807b64a4efa618adb2952d26a470c5879
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-11-07 13:48:24 -0300

    ajuste db

--- src/config/data_base.py
+++ src/config/data_base.py
@@ -1,16 +1,30 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
+from urllib.parse import quote_plus
 
 db = SQLAlchemy()
 
 def init_db(app):
     """
     Inicializa a base de dados com o app Flask e o SQLAlchemy.
+    Suporta PostgreSQL (como Supabase) e SQLite local.
     """
-    database_url = os.environ.get("DATABASE_URL")
-    if not database_url:
-        raise Exception("Variável de ambiente DATABASE_URL não definida")
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        parts = database_url.split("@")
+        if len(parts) == 2 and ":" in parts[0]:
+            user_pass, host_port_db = parts
+            user, password = user_pass.split(":", 1)
+            password = quote_plus(password)  
+            database_url = f"{user}:{password}@{host_port_db}"
+
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{database_url}"
+    else:
+        
+        base_dir = os.path.abspath(os.path.dirname(__file__))
+        db_path = os.path.join(base_dir, "market_management.db")
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
