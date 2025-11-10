184243374852305d46bed515667abe74607722e1
Marcelo Augusto Ribeiro Martins <marcelo0204martins@gmail.com>
2025-11-10 18:10:24 -0300

    venda

--- src/Application/Controllers/produto_controller.py
+++ src/Application/Controllers/produto_controller.py
@@ -251,7 +251,7 @@ class ProdutoController:
                 "id": nova_venda.id,
                 "produto_id": nova_venda.produto_id,
                 "quantidade": nova_venda.quantidade,
-                "data_venda": nova_venda.data.strftime("%Y-%m-%d %H:%M:%S")  # se houver campo datetime
+                "data_venda": nova_venda.data_venda.strftime("%Y-%m-%d %H:%M:%S")
             }
 
             return jsonify({"sucesso": "Produto vendido com sucesso!", "nova_venda": venda_dict})
